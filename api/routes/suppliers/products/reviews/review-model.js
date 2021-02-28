eeef7bd9c49cb530e67ac42bdb62a7a764d7a368
Diego Lopes <diego-chez@hotmail.com>
2021-02-28 10:29:45 -0300

    create reviews table

---
+++ api/routes/suppliers/products/reviews/review-model.js
@@ -0,0 +1,39 @@
+const Sequelize = require("sequelize");
+const instance = require("../../../../database");
+const productModel = require("../product-model")
+
+const columns = {
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descricao: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    nota: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        min: 0,
+        max: 5
+    },
+    idProduto: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: productModel,
+            key: "id"
+        }
+    }
+}
+
+const options = {
+    freezeTableName: true,
+    tableName: "avaliacoes",
+    timestamps: true,
+    createdAt: "data_criacao",
+    updatedAt: "data_atualizacao",
+    version: "versao",
+};
+  
+module.exports = instance.define("avaliacao", columns, options);
